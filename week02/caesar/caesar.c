a68f41dce1cdaa4c62150f0c9651047233e1881f
alber <aicarq@hotmail.com>
2025-08-01 10:08:33 -0600

    not yet converted to integer

--- week02/caesar/caesar.c
+++ week02/caesar/caesar.c
@@ -7,7 +7,7 @@
 #include <stdint.h>
 
 int const totalLetters = 26; 
-
+int onlyDigits(string zeroToNine);
 int const abecedarian[] = {
     0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
     0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70,
@@ -37,11 +37,12 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    int key = argv[1];
+    // argv[1][0] = tolower(argv[1][0]);
+    string key = argv[1];
 
     printf("argc: %d\n", argc);
     printf("argv[0]: %s\n", argv[0]);
-    printf("argv[1]: %d\n", key);    
+    printf("argv[1]: %s\n", key);    
 
     // int key2 = strtoimax(argv[1], NULL, 10);
     // int remainder = key % totalLetters;
@@ -50,3 +51,15 @@ int main(int argc, string argv[])
     // printf("plaintext: %s\n", plainText);
 
 };
+
+int onlyDigits(string zeroToNine)
+{
+    for (int i = 0, n = strlen(zeroToNine); i < n; i++)
+    {
+        if (!isdigit(zeroToNine[i]))
+        {
+            return 0; // Not all characters are digits
+        }
+    }
+    return 1; // All characters are digits
+}
