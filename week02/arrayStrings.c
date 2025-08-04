95516cdd1b27086c2516a2c2af065e5effbadf02
alber <aicarq@hotmail.com>
2025-08-04 15:06:12 -0600

    ciphertext function added

--- week02/arrayStrings.c
+++ week02/arrayStrings.c
@@ -4,12 +4,16 @@
 char src[] = "hello";
 char dest[100]; // Make sure dest is large enough
 
-for (int i = 0; src[i] != '\0'; i++)
+int main(void)
 {
-    dest[i] = src[i];
-    // Optionally, add a null terminator at the end
-    dest[i + 1] = '\0';
-}
-// Now dest contains a copy of src
+    for (int i = 0; src[i] != '\0'; i++)
+    {
+        dest[i] = src[i];
+        // Optionally, add a null terminator at the end
+        dest[i + 1] = '\0';
+    }
+    // Now dest contains a copy of src
+    
+    // ...existing code...
 
-// ...existing code...
+}
